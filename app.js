78616b55a0354e9c321ba69cf451f6062ee92311
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2022-07-29 01:53:53 +0530

    API  source changed to OpenWeatherMap
    The Dark Sky API service stopped recently ,changed the  API to Open Weather Map API.

--- app.js
+++ app.js
@@ -6,51 +6,52 @@ window.addEventListener('load',()=>{
   let locationTimezone = document.querySelector('.location-timezone');
   let temperatureSection = document.querySelector('.temperature');
   let temperatureSpan = document.querySelector('.temperature span');
-
+  let icon=document.querySelector('.icon');
+  loc= document.getElementById('cityname');
   if(navigator.geolocation){
 
     navigator.geolocation.getCurrentPosition(position=>{
-      long = position.coords.longitude;
+      lon = position.coords.longitude;
       lat = position.coords.latitude;
-
-      const proxy = "https://cors-anywhere.herokuapp.com/"
-      const api = `${proxy}https://api.darksky.net/forecast/988a2a12b3928df736cd950ca8f974d5/${lat},${long}`
-
-      fetch(api)
+      const APIkey='a0afe73789e4305f8b69bd74bab60c64';
+      const api=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}`; 
+      
+   const geoAPI=`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${5}&appid=${APIkey}`;    
+   fetch(geoAPI).then(res=>res.json()).then(data =>{
+    console.log(data[0].name,data[0].country);  
+    loc.innerHTML=`${data[0].name}, ${data[0].country}`;
+   });   
+   fetch(api)
        .then(response=>{
         return response.json();
       })
       .then(data=>{
         console.log(data)
-        const {temperature,summary,icon}  = data.currently;
+        const temperature=Number(data.current.temp - 273.15).toFixed(0);
+        const farhenheit = (temperature * 9/5) + 32;
+        //const icon=`http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
+        const summary  = data.current.weather[0].main;
         temperatureDegree.textContent = temperature;
+        temperatureSpan.innerHTML="°C"
         temperatureDescription.textContent = summary;
         locationTimezone.textContent = data.timezone;
         //formula for celcius
-        let celcius = (temperature-32)*(5/9);
-
-        setIcon(icon, document.querySelector(".icon"));
+        
+        icon.src=`http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
 
         temperatureSection.addEventListener("click",()=>{
           if(temperatureSpan.textContent=== "°F"){
             temperatureSpan.textContent = "°C";
-            temperatureDegree.textContent = Math.floor(celcius);
+            temperatureDegree.textContent = temperature;
           }
           else {
             temperatureSpan.textContent = "°F";
-            temperatureDegree.textContent = temperature;
+            temperatureDegree.textContent = farhenheit;
           }
         });
 
         })
-        function setIcon(icon,iconID){
-        const skycons = new Skycons({"color": "white"});
-        const currentIcon = icon.replace(/-/g,"_").toUpperCase();
-        skycons.play();
-        return skycons.set(iconID,Skycons[currentIcon]);
-      }
     })
   }
 
 })
-window.navigator.vibrate(pattern);
